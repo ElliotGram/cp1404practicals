e044cfeae32f9a666b6f118034621065431bf6fd
Elliot <elliot.gram@learn.jcu.edu.au>
2023-09-24 20:38:55 +1000

    scores tracker

--- scores.py
+++ scores.py
@@ -2,20 +2,25 @@
 CP1404/CP5632 - Practical
 Scores
 """
-score = float(input("Enter score: ")
-              )
-if 0 >= score or score > 100:
+import random
 
-print("Invalid score")
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
-score = float(input("Enter score: ")
-              )
-if score >= 90:
+def main():
+    user_score = float(input("Enter your score: "))
+    result = determine_result(user_score)
+    print(result)
 
-print("Excellent")
-elif score >= 50:
+    random_score = random.randint(0, 100)
+    result = determine_result(random_score)
+    print(f"Random score ({random_score}): {result}")
 
-print("Passable")
-else:
-
-print("Bad")
+main()
