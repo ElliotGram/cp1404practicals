8c17088435a1ccbfc75185f41a1df1c1ca41fff4
Elliot <elliot.gram@learn.jcu.edu.au>
2023-09-24 20:37:49 +1000

    scores tracker

---
+++ scores.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 - Practical
+Scores
+"""
+score = float(input("Enter score: ")
+              )
+if 0 >= score or score > 100:
+
+print("Invalid score")
+
+score = float(input("Enter score: ")
+              )
+if score >= 90:
+
+print("Excellent")
+elif score >= 50:
+
+print("Passable")
+else:
+
+print("Bad")
