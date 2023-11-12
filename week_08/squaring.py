f76dd2c14bf2279ad09e8b27870607f3993acf06
Elliot <elliot.gram@learn.jcu.edu.au>
2023-11-12 23:16:03 +1000

    Changed back author as no changes were made

--- week_08/squaring.py
+++ week_08/squaring.py
@@ -1,15 +1,15 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Elliot Gram, IT@JCU
-Started 12/11/2023
+Lindsay Ward, IT@JCU
+Started 13/10/2015
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Elliot Gram'
+__author__ = 'Lindsay Ward'
 
 
 class SquareNumberApp(App):
