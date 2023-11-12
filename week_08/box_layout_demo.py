fdbc70f220ad9a956e6d15211ebef0b960dd2521
Elliot <elliot.gram@learn.jcu.edu.au>
2023-11-12 21:13:53 +1000

    Copy prac 8 files

---
+++ week_08/box_layout_demo.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        if random.randint(1, 10) <= 5:
+            self.root.ids.my_label.text = "ouch!!"
+        else:
+            self.root.ids.my_label.text = "stop that!!"
+
+
+BoxLayoutDemo().run()
