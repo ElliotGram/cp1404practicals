6e5d1d711a02b230137b21547bc21c92b9b062cb
Elliot <elliot.gram@learn.jcu.edu.au>
2023-11-12 23:39:17 +1000

    Upload base outlines of models

--- week_08/ dynamic_labels.py
+++ week_08/ dynamic_labels.py
@@ -1,10 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+
 class DynamicLabelsApp(App):
     names = ['Alice', 'Bob', 'Charlie', 'David']
 
     def build(self):
         return Builder.load_file('dynamic_labels.kv')
 
+
+print('test')
 DynamicLabelsApp().run()
