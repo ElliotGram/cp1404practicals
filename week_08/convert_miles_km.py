d3ffdd97570237446793d0656e640ac73028a345
Elliot <elliot.gram@learn.jcu.edu.au>
2023-11-12 23:22:41 +1000

    Added Class

--- week_08/convert_miles_km.py
+++ week_08/convert_miles_km.py
@@ -3,3 +3,30 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 MILES_TO_KM_CONVERSION = 1.60934  # Constant for conversion factor
+
+
+class MilesToKmConverter(App):
+    output_result = StringProperty("0.0")
+
+    def build(self):
+        return Builder.load_file('convert_miles_km.kv')
+
+    def handle_conversion(self):
+        try:
+            miles = float(self.root.ids.input_miles.text)
+            kilometers = miles * MILES_TO_KM_CONVERSION
+            self.output_result = str(kilometers)
+        except ValueError:
+            self.output_result = "0.0"
+
+    def handle_increment(self, value):
+        try:
+            miles = float(self.root.ids.input_miles.text)
+        except ValueError:
+            miles = 0.0
+        miles += value
+        self.root.ids.input_miles.text = str(miles)
+        self.handle_conversion()
+
+
+MilesToKmConverter().run()
