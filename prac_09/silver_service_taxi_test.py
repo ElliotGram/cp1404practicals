bc4592811e03ef99933470417e3694e74e7245fe
Elliot <elliot.gram@learn.jcu.edu.au>
2023-11-19 23:13:51 +1000

    silver service taxi test content completed

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,13 @@
 CP1404/CP5632 Practical
 silver service taxi
 """
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+# Create a new SilverServiceTaxi object, my_taxi, with name "Hummer", 200 units of fuel, and fanciness of 4
+my_taxi = SilverServiceTaxi("Hummer", 200, 4)
+
+# Drive the taxi 18 km
+my_taxi.drive(18)
+
+# Print the taxi's details and the current fare
+print(my_taxi)
